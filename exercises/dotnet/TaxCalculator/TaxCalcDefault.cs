7561f8c5199d4a67d5fd9d41c167f457e3e14397
shujat <khalidshujat@gmail.com>
2020-01-20 13:42:01 +0000

    refactored

--- exercises/dotnet/TaxCalculator/TaxCalcDefault.cs
+++ exercises/dotnet/TaxCalculator/TaxCalcDefault.cs
@@ -9,55 +9,111 @@ namespace TaxCalculator
         public override int CalculateTax(Vehicle vehicle)
         {
             //vehicle.Co2Emissions
+            //if (vehicle.Co2Emissions == 0)
+            //{
+            //    return 0;
+            //}
+            //else if (vehicle.Co2Emissions >= 1 && vehicle.Co2Emissions <= 50)
+            //{
+            //    return 10;
+            //}
+            //else if (vehicle.Co2Emissions >= 51 && vehicle.Co2Emissions <= 75)
+            //{
+            //    return 25;
+            //}
+            //else if (vehicle.Co2Emissions >= 76 && vehicle.Co2Emissions <= 90)
+            //{
+            //    return 105;
+            //}
+            //else if (vehicle.Co2Emissions >= 91 && vehicle.Co2Emissions <= 100)
+            //{
+            //    return 125;
+            //}
+            //else if (vehicle.Co2Emissions >= 101 && vehicle.Co2Emissions <= 110)
+            //{
+            //    return 145;
+            //}
+            //else if (vehicle.Co2Emissions >= 111 && vehicle.Co2Emissions <= 130)
+            //{
+            //    return 165;
+            //}
+            //else if (vehicle.Co2Emissions >= 131 && vehicle.Co2Emissions <= 150)
+            //{
+            //    return 205;
+            //}
+            //else if (vehicle.Co2Emissions >= 151 && vehicle.Co2Emissions <= 170)
+            //{
+            //    return 515;
+            //}
+            //else if (vehicle.Co2Emissions >= 171 && vehicle.Co2Emissions <= 190)
+            //{
+            //    return 830;
+            //}
+            //else if (vehicle.Co2Emissions >= 191 && vehicle.Co2Emissions <= 225)
+            //{
+            //    return 1240;
+            //}
+            //else if (vehicle.Co2Emissions >= 191 && vehicle.Co2Emissions <= 225)
+            //{
+            //    return 1240;
+            //}
+            //else if (vehicle.Co2Emissions >= 226 && vehicle.Co2Emissions <= 255)
+            //{
+            //    return 1760;
+            //}
+            //else if (vehicle.Co2Emissions > 255)
+            //{
+            //    return 2070;
+            //}
             if (vehicle.Co2Emissions == 0)
             {
                 return 0;
             }
-            else if (vehicle.Co2Emissions >= 1 && vehicle.Co2Emissions <= 50)
+            else if (vehicle.Co2Emissions <= 50)
             {
                 return 10;
             }
-            else if (vehicle.Co2Emissions >= 51 && vehicle.Co2Emissions <= 75)
+            else if (vehicle.Co2Emissions <= 75)
             {
                 return 25;
             }
-            else if (vehicle.Co2Emissions >= 76 && vehicle.Co2Emissions <= 90)
+            else if (vehicle.Co2Emissions <= 90)
             {
                 return 105;
             }
-            else if (vehicle.Co2Emissions >= 91 && vehicle.Co2Emissions <= 100)
+            else if (vehicle.Co2Emissions <= 100)
             {
                 return 125;
             }
-            else if (vehicle.Co2Emissions >= 101 && vehicle.Co2Emissions <= 110)
+            else if (vehicle.Co2Emissions <= 110)
             {
                 return 145;
             }
-            else if (vehicle.Co2Emissions >= 111 && vehicle.Co2Emissions <= 130)
+            else if (vehicle.Co2Emissions <= 130)
             {
                 return 165;
             }
-            else if (vehicle.Co2Emissions >= 131 && vehicle.Co2Emissions <= 150)
+            else if (vehicle.Co2Emissions <= 150)
             {
                 return 205;
             }
-            else if (vehicle.Co2Emissions >= 151 && vehicle.Co2Emissions <= 170)
+            else if (vehicle.Co2Emissions <= 170)
             {
                 return 515;
             }
-            else if (vehicle.Co2Emissions >= 171 && vehicle.Co2Emissions <= 190)
+            else if (vehicle.Co2Emissions <= 190)
             {
                 return 830;
             }
-            else if (vehicle.Co2Emissions >= 191 && vehicle.Co2Emissions <= 225)
+            else if (vehicle.Co2Emissions <= 225)
             {
                 return 1240;
             }
-            else if (vehicle.Co2Emissions >= 191 && vehicle.Co2Emissions <= 225)
+            else if (vehicle.Co2Emissions <= 225)
             {
                 return 1240;
             }
-            else if (vehicle.Co2Emissions >= 226 && vehicle.Co2Emissions <= 255)
+            else if (vehicle.Co2Emissions <= 255)
             {
                 return 1760;
             }
